2ed4864f14df75f7c3d5eb580470d3b43b7cd90b
madhankumarmmk <madhankumar_muthusamy@srmuniv.edu.in>
2017-07-19 10:18:11 -0700

    Add files via upload

---
+++ even or odd.java
@@ -0,0 +1,22 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int x;
+System.out.println("Enter a number");
+Scanner in = new Scanner(System.in);
+x = in.nextInt();
+if(x%2==0)
+
+System.out.println("It is even");
+
+else 
+
+System.out.println("It is odd");
+
+
+
+ 
+ 
+	}
+}
