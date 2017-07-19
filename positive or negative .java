20f4604febfe989d68a52b45e3fc167095462ccb
madhankumarmmk <madhankumar_muthusamy@srmuniv.edu.in>
2017-07-19 09:23:03 -0700

    Add files via upload

---
+++ positive or negative .java
@@ -0,0 +1,25 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int x;
+System.out.println("Enter a number");
+Scanner in = new Scanner(System.in);
+x = in.nextInt();
+if(x<0)
+
+System.out.println("It is negative");
+
+else if(x == 0)
+
+System.out.println("It is zero");
+
+else
+
+System.out.println("It is positive");
+
+ 
+ 
+	
+	}
+}
